76604258811bbaec66d758117f56895fffe04970
Leeladhar <52621705+LeeladharRao@users.noreply.github.com>
2021-09-09 23:34:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
