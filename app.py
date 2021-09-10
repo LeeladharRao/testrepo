995a35fee9382151f2b58f0f10ccdf9af078398c
Leeladhar <52621705+LeeladharRao@users.noreply.github.com>
2021-09-10 12:39:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,5 +26,22 @@ def blog():
 def blog():
     return render_template('blog.html')
 
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
